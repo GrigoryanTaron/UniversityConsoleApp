7e235ede172f7343c5a953828926d2dd79eddfb9
Taron Grigoryan <1991taron@gmail.com>
2022-05-08 19:47:30 +0400

    Add Group Class

---
+++ UniversityConsoleApp/UniversityConsoleApp/UniversityConsoleApp/BL/Group.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UniversityConsoleApp.Models;
+
+namespace UniversityConsoleApp.BL
+{
+    public class Group
+    {
+        public Group()
+        {
+
+        }
+        public Group(Student[] students)
+        {
+            _id = Guid.NewGuid();
+            Console.WriteLine($"Students Group {_id}");
+            StudentManager.Print(students);
+        }
+        public Group(Teacher[] teachers)
+        {
+            _id = Guid.NewGuid();
+            Console.WriteLine($"Teachers Group {_id} ");
+            TeacherManager.Print(teachers);
+        }
+        public Guid _id;
+
+    }
+}
