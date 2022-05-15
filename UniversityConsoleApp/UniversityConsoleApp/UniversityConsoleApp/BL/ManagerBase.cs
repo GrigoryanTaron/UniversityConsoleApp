0889bdf80e83167f0225ce1534a61c7d09acd76e
Taron Grigoryan <1991taron@gmail.com>
2022-05-15 12:07:28 +0400

    Add manager base

--- UniversityConsoleApp/UniversityConsoleApp/UniversityConsoleApp/BL/ManagerBase.cs
+++ UniversityConsoleApp/UniversityConsoleApp/UniversityConsoleApp/BL/ManagerBase.cs
@@ -5,12 +5,30 @@ namespace UniversityConsoleApp.BL
 {
     public class ManagerBase
     {
-        public string Person Create(string firstName, string lastName, int age)
+        const short MaxAge = 70;
+        public virtual Person Create(string firstName, string lastName, int age)
             => new Person()
             {
                 FirstName = firstName,
                 LastName = lastName,
                 Age = age,
             };
+        public virtual Person[] Create(int count,int minage)
+        {
+            Person[] persons = new Person[count];
+            Random rand = new Random();
+            for (int i = 0; i < persons.Length; i++)
+            {
+                persons[i] = new Person()
+                {
+                    FirstName = $"firsname{i}",
+                    LastName = $"lastname{i}",
+                    Age = rand.Next(minage, MaxAge),
+
+                };
+            }
+            return persons;
+        }
+
     }
 }
